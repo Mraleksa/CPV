8c96466f83c73f15348fb97fb2af8378c9910335
Mraleksa <a.gribanovskiy@gmail.com>
2017-06-01 22:24:45 +0300

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -44,7 +44,7 @@ db.serialize(function() {
   // Insert a new record
   var statement = db.prepare("INSERT INTO data VALUES (?,?)");
   
-  statement.run(data.getJSON().data.items[0].classification.description,data.getJSON().data.items[0].classification.id);
+  statement.run(data.getJSON().data.items[0].classification.id,data.getJSON().data.items[0].classification.id);
  
  console.log(data.getJSON().data.items[0].classification.id)
   //else none;
